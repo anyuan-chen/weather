5a55c9d7f8eb74ef44d4c23de5c875372f37764e
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-04-13 20:10:25 -0400

    error checking for empty params bad input

--- cmd/weather/main.go
+++ cmd/weather/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"github.com/joho/godotenv"
 )
 
 type weatherData struct {
@@ -13,17 +17,40 @@ type weatherData struct {
 }
 
 func queryOpenWeather(city string)(weatherData, error){
-	apiCall := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid={API key}", city, )
-
-	response, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}")
+	apiKey := os.Getenv("OPENWEATHERMAP_API_KEY") 
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", city, apiKey)
+	response, err := http.Get(url)
+	if err != nil {
+		return weatherData{}, err
+	}
+	defer response.Body.Close()
+	var recievedData weatherData
+	if err := json.NewDecoder(response.Body).Decode(&recievedData); err != nil {
+		return weatherData{}, err
+	}
+	return recievedData, nil
 }
 
 func getWeather(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("hi"))
+	params := r.URL.Query()
+	if len(params["city"]) == 0 {
+		http.Error(w, "invalid request", http.StatusInternalServerError)
+		return
+	}
+	openWeatherData, openWeatherError := queryOpenWeather(params["city"][0])
+	if openWeatherError != nil {
+		http.Error(w, openWeatherError.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(openWeatherData)
 }
 
 func main(){
-	fmt.Println("hi")
+	enverr := godotenv.Load()
+	if (enverr != nil){
+		log.Fatal("Error loading the .env file")
+	}
 	http.HandleFunc("/weather", getWeather)
 	http.ListenAndServe(":8080", nil)
 }
